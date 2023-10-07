3ca36e8c0f0df2948317f1376c43157cc1ed0e48
Amagyei <Kwame.na@hotmail.com>
2023-10-07 07:27:52 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  * main - Entry point
  *
  * Description : 'printing whether random number is negative positive or zer' 
- * 
+ *
  * Return: Always 0 (success)
  */
 int main(void)
@@ -19,8 +19,8 @@ int main(void)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	else if (n < 0) 
+	else if (n < 0)
 		printf("%d is negative\n", n);
 	return (0);
-       /*thsi does it*/	
+       /*thsi does it*/
 }
