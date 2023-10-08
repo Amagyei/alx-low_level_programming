423e7a0763f0745b5b160f372ec2adc9e0b99866
Amagyei <Kwame.na@hotmail.com>
2023-10-08 16:12:40 -0700

    task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Description : 'the code is to print all the letters of the alphabet'
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	/* initializing the variable */
+	int a = '0';
+	int b = '0';
+
+	while (a <= '9')
+	{
+		while (b <= '9')
+		{
+			if (!(a > b || a == b))
+			{
+				putchar(a);
+				putchar(b);
+
+		 		if (a == 8 && b == 9)
+					putchar('\n');
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}			
+			}
+			b++;
+		}
+		a++;		
+	}
+	return (0);
+
+}
