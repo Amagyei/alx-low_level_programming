c83c61321765d894eae9aab8a442799a70b0fc07
Amagyei <Kwame.na@hotmail.com>
2023-10-08 12:39:35 -0700

    task 3 is complete

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description : 'Code to print tht alphabet'
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter != 'Z')
+	{
+		putchar(letter);
+		letter++;
+		if (letter == 'z')
+			letter = 'A';
+	}
+	putchar('\n');
+	return (0);
+}
+
