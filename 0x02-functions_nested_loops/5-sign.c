5819c29e46fcac7eb508916db1f8ff1e07146ae2
amagyei <kwame.na@hotmail.com>
2023-10-10 23:46:24 +0000

    try

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_alphabet_x10 - program that print alphabet lowercase
+ *
+ * _islower - A program that checks for lowercase.
+ *
+ *  @c: A parameter to check for lowercase
+ * Return: Always 0
+ */
+
+int print_sign(int n)
+{
+int a = 1;
+int b = 0;
+int c = -1;
+if (n > 0)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar(48);
+return (0);
+}
+else
+{
+_putchar('-');
+}
+return (-1);}
+}
