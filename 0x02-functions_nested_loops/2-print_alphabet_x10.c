6a7c56be950b05077627434e0728449bf00c66b7
amagyei <kwame.na@hotmail.com>
2023-10-10 22:45:23 +0000

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - function that print alphabets in lower case
+ *
+ * Return: always 0;
+ */
+
+void print_alphabet_x10(void)
+{
+    for (int i = 0 ; i < 10 ; i++ ){
+        print_alphabet()
+    }
+}
