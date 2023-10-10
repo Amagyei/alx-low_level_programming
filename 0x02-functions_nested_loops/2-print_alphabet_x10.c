b68a1f31954cb285b29eff77da3c2f66bc60cbb4
amagyei <kwame.na@hotmail.com>
2023-10-10 22:50:48 +0000

    task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,8 @@
 
 void print_alphabet_x10(void)
 {
-    for (int i = 0 ; i < 10 ; i++ ){
-        print_alphabet()
+    int i;
+    for ( i = 0 ; i < 10 ; i++ ){
+        print_alphabet();
     }
 }
