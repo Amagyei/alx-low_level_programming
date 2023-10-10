9a83846136adbf4327310df44be402b45ae6c111
amagyei <kwame.na@hotmail.com>
2023-10-10 23:01:50 +0000

    t

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,17 @@
 
 void print_alphabet_x10(void)
 {
-    int i;
-    for ( i = 0 ; i < 10 ; i++ ){
-       char a;
+int i;
+
+for ( i = 0 ; i < 10 ; i++ )
+{
+    char a;
 
 	for (a = 'a' ; a <= 'z' ; a++)
-		_putchar(a);
-	    _putchar('\n');
+	{
+        _putchar(a);
+	
+        _putchar('\n');
     }
+}
 }
