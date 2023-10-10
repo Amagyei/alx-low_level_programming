f5b3414cadfe35d25d9946cd65f378cff51bf3fe
amagyei <kwame.na@hotmail.com>
2023-10-10 21:38:13 +0000

    task 0 done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - Entry point
+ *
+ * Description : 'printing whether random number is negative positive or zer'
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 puts("_putchar");
