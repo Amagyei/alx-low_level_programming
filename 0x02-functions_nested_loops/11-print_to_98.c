e1b7f390ffae52cd46ecaf3419e2d001cfd4e71e
amagyei <kwame.na@hotmail.com>
2023-10-11 09:54:03 +0000

    task

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description : 'printing whether random number is negative positive or zer'
+ *
+ * Return: Always 0 (success)
+ */
+void print_to_98(int n)
+{
+while (n != 98){
+    if (n > 98)
+    {
+        printf(n);
+        putchar(', ')
+        n--
+    }
+    else {
+        printf(n);
+        putchar(', ')
+        n++
+    }
+    return (0)
+}
+}
