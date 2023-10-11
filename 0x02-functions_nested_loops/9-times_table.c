6c33bd34883366cfbc009c701aada490f83c5b5f
amagyei <kwame.na@hotmail.com>
2023-10-11 08:59:49 +0000

    task

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -33,6 +33,8 @@ void times_table(void)
             _putchar('0' + a % 10);
             _putchar(',');
             _putchar(' ');
+            _putchar(' ');
+
             }
             
             j++;
