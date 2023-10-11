18c17f8a8d28de61ee7c85936a2fb533100e45ce
amagyei <kwame.na@hotmail.com>
2023-10-11 13:40:01 +0000

    task @

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * Description : 'Hopefully it all works'
+ * Return: ALways 0 (Success)
  */
 void times_table(void)
 {
@@ -12,7 +13,7 @@ while (i < 10)
 j = 0;
 while (j < 10)
 {
-k = j * i;
+k = j *i;
 	if (j == 0)
 	{
 		_putchar(k + '0');
