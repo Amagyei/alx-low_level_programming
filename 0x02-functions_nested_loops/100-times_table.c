08af455557314c03cf53719df6f35bdf837e24ae
amagyei <kwame.na@hotmail.com>
2023-10-11 11:07:44 +0000

    tas

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ */
+void times_table(lim)
+{
+	int i, j, k;
+
+	i = 0;
+
+	while (i < lim)
+	{
+		j = 0;
+
+		while (j < lim)
+		{
+        k = j * i;
+
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+        j++;
+        }
+        _putchar('\n');
+		i++;
+	}
+}
