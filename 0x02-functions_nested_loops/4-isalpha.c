73c80a8cf89f09557afb338091f42a53a865d24a
amagyei <kwame.na@hotmail.com>
2023-10-11 14:01:15 +0000

    pus

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,8 +2,7 @@
 #include <ctype.h>
 
 /**
- * Description : program that print alphabet lowercase
- *
+ * Description : 'Hopefully it all works'
  * _isalpha - A program that checks for lowercase.
  *
  * @c: A parameter to check for lowercase
