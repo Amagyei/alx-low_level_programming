b2e67a24a82e3d289ee8464094579f78f0aca014
amagyei <kwame.na@hotmail.com>
2023-10-11 13:27:18 +0000

    tr

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 /**
  * add - Entry point
- *
+ * @a : this thing
  * Description : 'printing whether random number is negative positive or zer'
- *
+ * @b : this thing
  * Return: Always 0 (success)
  */
 int add(int a, int b)
