52a0fd5ac351bdf65a357de9e3d1309fbe655e0c
amagyei <kwame.na@hotmail.com>
2023-10-12 23:24:04 +0000

    task k

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * mul - Entry point
+ * @a : this thing
+ * Description : 'printing whether random number is negative positive or zer'
+ * @b : this thing
+ * Return: Always 0 (success)
+ */
+int void print_numbers(void)
+{
+
+return (a * b);
+}
