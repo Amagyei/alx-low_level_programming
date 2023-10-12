4d1f327a7223d49e0bff34316d7c000db14df551
amagyei <kwame.na@hotmail.com>
2023-10-12 21:42:18 +0000

    git push

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower - A program that checks for lowercase.
+ * Description : 'printing whether random number is negative positive or zer'
+ * @c: A parameter to check for lowercase
+ * Return: Always 0
+ */
+
+int _isupper(int c)
+{
+int a = isupper(c);
+if (a != 0)
+{
+a = 1;
+}
+return (a);
+}
