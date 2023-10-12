cd277dff2264cc9e368467e767fdcb8df1f71bf8
amagyei <kwame.na@hotmail.com>
2023-10-12 22:41:03 +0000

    task

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -12,24 +12,3 @@ int _isdigit(int c)
 {
 	return (c >= '0' && c <= '9');
 }
-// int _isdigit(int c)
-// {
-// int a = isdigit(c);
-// if (a != 0)
-// {
-// a = 1;
-// }
-// else
-// a = 0;
-// return (a);
-// }
-// // int main(void)
-// // {
-// //     char c;
-
-// //     c = '0';
-// //     printf("%c: %d\n", c, _isdigit(c));
-// //     c = 'a';
-// //     printf("%c: %d\n", c, _isdigit(c));
-// //     return (0);
-// // }
