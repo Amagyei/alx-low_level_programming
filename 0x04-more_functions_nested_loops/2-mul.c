5ec4062d42f48c8488cf403c3a2bb8a18aa3134e
amagyei <kwame.na@hotmail.com>
2023-10-12 23:05:37 +0000

    tas

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * add - Entry point
+ * @a : this thing
+ * Description : 'printing whether random number is negative positive or zer'
+ * @b : this thing
+ * Return: Always 0 (success)
+ */
+int mul(int a, int b)
+{
+
+return (a * b);
+}
