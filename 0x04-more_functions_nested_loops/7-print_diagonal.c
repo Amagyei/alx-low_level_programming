980b60158b334bebd3af95848937168f129395e8
amagyei <kwame.na@hotmail.com>
2023-10-13 04:04:10 +0000

    tasks done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,22 +1,30 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * print_line - Entry point
- * Description : 'printing whether random number is negative positive or zer'
- * Return: Always 0 (success)
- *  * @n : this thing
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
  */
 
 void print_diagonal(int n)
 {
-int i;
+if (n <= 0)
+{
+_putchar('\n');
+} else
+{
+int i, j;
 for (i = 0; i < n; i++)
 {
+for (j = 0; j < n; j++)
+{
+if (j == i)
 _putchar('\\');
+else if (j < i)
+_putchar(' ');
+}
 _putchar('\n');
 }
-
 }
+}
+
 
