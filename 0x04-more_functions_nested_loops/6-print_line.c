f2a026da09b3546875f1b787444abccf0568ed92
amagyei <kwame.na@hotmail.com>
2023-10-13 01:21:30 +0000

    task

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * Description : 'printing whether random number is negative positive or zer'
+ * Return: Always 0 (success)
+ */
+
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_');
+}
+}
