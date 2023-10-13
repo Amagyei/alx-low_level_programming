7aa020846903a6aab260443a56239ce97d6cb0a9
amagyei <kwame.na@hotmail.com>
2023-10-13 00:22:03 +0000

    git push

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,6 +12,7 @@ void more_numbers(void)
     for (i = 0; i < 11; i++){
         for (j = 0; j < 15; j++)
         {
+            _putchar(j / 10 + '0');
             _putchar(j + '0');
         }
         _putchar('\n');
