123eeb6d054d094253fabfee5bf1e293e3d519fd
Amagyei <Kwame.na@hotmail.com>
2023-10-12 20:43:52 -0700

    zdf

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+
 #include <stdlib.h>
 #include "main.h"
 
@@ -13,7 +13,7 @@ void print_diagonal(int n){
     int i;
     for (i = 0; i < n; i++)
     {
-        _putchar("\\");
+        _putchar('\\');
     }
     _putchar('\n');
 
