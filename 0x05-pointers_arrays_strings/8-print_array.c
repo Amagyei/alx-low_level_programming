91546c5def25b2e10c788243b19405bdb8a46758
amagyei <kwame.na@hotmail.com>
2023-10-16 23:27:50 +0000

    task 7

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,11 @@
+
+/**
+ * _strlen - check the code
+ *
+ * Descriptit on : 'check the code'
+ *
+ * @s: A parameter to check for lowercase
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n);
