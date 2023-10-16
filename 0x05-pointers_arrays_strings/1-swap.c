6b942e0b51717edd390015f13d1d5155493e013f
amagyei <kwame.na@hotmail.com>
2023-10-16 18:11:35 +0000

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code
+ *
+ * Description : 'check the code'
+ *
+ * @n: A parameter to check for lowercase
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b);
+{
+int temp = *a;
+
+*a = *b;
+*b = *a;
+}
