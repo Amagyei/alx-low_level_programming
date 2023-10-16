7fe363da19ef4df56482cb79f619d6b629ac1ba4
amagyei <kwame.na@hotmail.com>
2023-10-16 18:22:10 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code
+ *
+ * Description : 'check the code'
+ *
+ * @s: A parameter to check for lowercase
+ * Return: Always 0.
+ */
+
+void _strlen(char *s)
+{
+return(len *s)
+}
