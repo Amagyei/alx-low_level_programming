d289f252ad1258214765e06074c9cc04b9ca6829
amagyei <kwame.na@hotmail.com>
2023-10-18 09:13:39 +0000

    task 0 comppleted

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * dest: The character to print
+ * src: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+
+char *_strcat(char *dest, char *src)
+{
+return(strcat(dest, src));
+}
