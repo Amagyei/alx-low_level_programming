88443c4e4a83b2609b628c1dff371ea95d5169c5
amagyei <kwame.na@hotmail.com>
2023-10-19 23:46:24 +0000

    task 100

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,18 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h> // Include this header for the strlen function
+
 /**
  * rot13 - change letters to ROT13.
- * @s: analized string.
+ * @s: analyzed string.
  *
- * Return: String with all letters in ROT13 base.
+ * Return: String with all letters in ROT13 encoding.
  */
 char *rot13(char *s)
 {
-	int i = 0;
-	for (i = 0; i < strlen(s); i++)
-    {
-        char a = *(s + i);
-        char a += 13;
+    int i;
+    for (i = 0; s[i] != '\0'; i++) {
+        char a = s[i];
+
+        if ((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z')) {
+            // Check if the character is an uppercase or lowercase letter
+            char base = (a >= 'A' && a <= 'Z') ? 'A' : 'a';
+            a = ((a - base + 13) % 26) + base;
+        }
+        s[i] = a; // Update the character in the input string
     }
-	return (s);
+    return s;
 }
