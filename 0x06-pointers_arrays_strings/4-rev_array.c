70dc14f3c3dd52b89f1f1bc972757960df3111ed
amagyei <kwame.na@hotmail.com>
2023-10-20 09:46:25 +0000

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,15 +9,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i = 0;
-    int j = n - 1;
-
-    while (i < j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-
-        i++;
-        j--;
-    }
+int i = 0;
+int j = n - 1;
+while (i < j) {
+int temp = a[i];
+a[i] = a[j];
+a[j] = temp;
+i++;
+j--;
+}
 }
