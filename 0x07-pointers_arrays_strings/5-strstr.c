3d0987f262b7ae7c1e19b52c2028543029753663
Amagyei <Kwame.na@hotmail.com>
2023-10-23 16:10:16 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,14 @@
+#include <string.h>
+/**
+ * _strstr - assign bytes of length b to string s
+ * Description : 'same thins '
+ * @needle: length of bytes
+ * @haystack :no of bytes
+ * Return: first somethiing
+ */
+
+char *_strsstr(char *haystack, char *needle)
+{
+	return (strstr(haystack, needle));
+}
+
