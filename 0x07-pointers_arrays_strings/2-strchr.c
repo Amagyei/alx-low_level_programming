5b0a6cc5036eb157a0a181ceee4eb6e79c0e0141
Amagyei <Kwame.na@hotmail.com>
2023-10-23 15:17:42 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include <string.h>
+/**
+ * _strchr - assign bytes of length b to string s
+ * Description : 'same thins '
+ * @c : the string
+ * @s : length of bytes
+ * Return: first somethiing
+ */
+
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
+
