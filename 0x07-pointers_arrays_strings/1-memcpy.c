bfc5194d09b10f7c603f82ca02533521a3801420
Amagyei <Kwame.na@hotmail.com>
2023-10-23 14:55:55 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+/**
+ * _memcpy - assign bytes of length b to string s
+ * Description : 'same thins '
+ * @src : the string
+ * @dest: length of bytes
+ * @n :no of bytes
+ * Return: first somethiing
+ */
+
+char *_memcpy(char *dest, char src, unsigned int n)
+{
+	return (memcpy(dest, src, n));
+
+}
+
