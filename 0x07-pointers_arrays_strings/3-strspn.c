81dde34154f87c4ca888f816fdc3437d80f8832a
Amagyei <Kwame.na@hotmail.com>
2023-10-23 15:30:36 -0700

    taks 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,15 @@
+#include <string.h>
+/**
+ * _strspn - assign bytes of length b to string s
+ * Description : 'same thins '
+ * @s : the string
+ * @accept: length of bytes
+ * Return: first somethiing
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int a = sizeof(strspan(s, accept));
+	return (a);
+}
+
