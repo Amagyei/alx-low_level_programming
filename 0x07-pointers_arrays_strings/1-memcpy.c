d406550dff2ce096a7568b792775250cd0358f57
Amagyei <Kwame.na@hotmail.com>
2023-10-23 15:03:53 -0700

    taks 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,9 +8,9 @@
  * Return: first somethiing
  */
 
-char *_memcpy(char *dest, char src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	memcpy(dest, src, n);
-	return (*dest);
+	return (dest);
 }
 
