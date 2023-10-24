393aed8e07d9e24d5a5c69cfc89ac49aeab0d9e9
amagyei <kwame.na@hotmail.com>
2023-10-24 11:03:32 +0000

    rtask 7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,15 +1,23 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * print_chessboard - assign bytes of length b to string s
+ * Description : 'same thins '
+ * @a :no of bytes
+ * Return: first somethiing
+ */
+
 void print_chessboard(char (*a)[8])
 {
 	int i, j;
+
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			printf("%c", char(a)[j]);
-		}	
+			printf("%c", a[i][j]);
+		}
 	}
 }
 
