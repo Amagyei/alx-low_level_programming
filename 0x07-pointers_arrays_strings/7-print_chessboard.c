5b9ddb54702d8721b9a7667cc58cf188a1cbc82e
Amagyei <Kwame.na@hotmail.com>
2023-10-24 04:26:33 -0700

    task & complete with betty

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -10,14 +10,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-	int i ,j = 0;
+	int i, j = 0;
 
 	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-            printf("%c", a[i][j]);
-        }
+			printf("%c", a[i][j]);
+		}
 		printf("\n");
 	}
 }
