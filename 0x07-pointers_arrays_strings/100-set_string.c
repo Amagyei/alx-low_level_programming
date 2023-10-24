e9ff46fc2391c20cbd217da529e8d7c567fbf73a
Amagyei <Kwame.na@hotmail.com>
2023-10-24 06:33:16 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_diagsums - assign bytes of length b to string s
+ * Description : 'same thins '
+ * @a :no of bytes
+ * @size :no of bytes
+ * Return: first somethiing
+ */
+
+void set_string(char **s, char *to)
+{
+	s = to
+}
+
