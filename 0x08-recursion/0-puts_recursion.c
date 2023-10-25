2cd4165b9735e4e6183e069a19c03b6c73a6e3b9
Amagyei <Kwame.na@hotmail.com>
2023-10-25 02:38:20 -0700

    task 0 trial

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/** 
+ * _puts_recursion - prints characters of a string 
+ *
+ * Description: 'Loops through the string to print each character'
+ *
+ * Return: voide
+ */
+
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	puts(s[i]);
+	Return;
+}
