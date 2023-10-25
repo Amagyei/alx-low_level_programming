f889d696a32b31d909cd7a5d421ddf9c35c17603
Amagyei <Kwame.na@hotmail.com>
2023-10-25 06:11:30 -0700

    task 4 try 1

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * factorial - print in reverse
+ *
+ * Description: ' Loopt through string and print revercse'
+ *
+ * @n: 'pointer to string to be printed'
+ *
+ * Return: void
+ */
+
+int _pow_recursion(int x, int y);
+{
+	if (y < 0)
+		return (-1);
+	if (y > 1)
+	{
+		return x = x * x;
+		y--;
+		_pow_recursion(x, y);
+	}
+	return (x);
+}
+
+
