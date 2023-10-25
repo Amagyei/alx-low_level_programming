d0647eeb65ca0afd3b8cc34d2103d63d48831157
Amagyei <Kwame.na@hotmail.com>
2023-10-25 05:30:56 -0700

    task 3 trial 1

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * factorial - print in reverse
+ *
+ * Description: ' Loopt through string and print revercse'
+ *
+ * @n: 'pointer to string to be printed'
+ *
+ * Return: void
+ */
+
+
+
+int factorial(int n);
+{
+	if (n != 1)
+	{	n = n * n - 1
+		return (n);
+	}
+}
+
+
