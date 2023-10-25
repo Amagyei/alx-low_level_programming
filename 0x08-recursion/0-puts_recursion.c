78f7ff7acff6ed599a803ba7b1d3d4be362021d7
Amagyei <Kwame.na@hotmail.com>
2023-10-25 02:52:25 -0700

    task 0 done

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 /*#include "main.h"*/
 
-/** 
- * _puts_recursion - prints characters of a string 
+/**
+ * _puts_recursion - prints characters of a string
  *
  * Description: 'Loops through the string to print each character'
+ *
+ * @s: pinter to string variable
  *
  * Return: voide
  */
@@ -13,9 +15,10 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0') 
+	if (*s != '\0')
 	{
 		putchar(*s);
-		_puts_recursion(s + 1); 
+		_puts_recursion(s + 1);
 	}
+	putchar('\n');
 }
