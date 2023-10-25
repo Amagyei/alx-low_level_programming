bfdda165fed565e28cc0b2bb452c4cf3826eec11
Amagyei <Kwame.na@hotmail.com>
2023-10-25 16:18:58 -0700

    task 4 try?

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,13 +15,11 @@
 
 int _pow_recursion(int x, int y)
 {
-	int b = x;
-	int *a = &x;
 	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	return x * _pow_recursion(x, y--)
+	return x * _pow_recursion(x, y--);
 }
 
 
