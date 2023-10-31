5433f030299f55eac02f81943d9377e34a5e5027
amagyei <kwame.na@hotmail.com>
2023-10-31 14:14:46 +0000

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,17 +3,17 @@
 #include <string.h>
 
 char *_strdup(char *str) {
+    char *duplicate
+
     if (str == NULL) {
-        return NULL;  // Return NULL if the input string is NULL.
+        return NULL;  .
     }
 
-    // Allocate memory for the duplicate string and check for allocation success.
-    char *duplicate = (char *)malloc(strlen(str) + 1);
+    duplicate = (char *)malloc(strlen(str) + 1);
 
     if (duplicate != NULL) {
-        // Copy the input string to the allocated memory.
         strcpy(duplicate, str);
     }
 
-    return duplicate;  // Return the pointer to the duplicated string.
+    return duplicate;
 }
