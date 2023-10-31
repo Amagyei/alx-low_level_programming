c88f888ebeb1dea13095b015c48f35210b053c33
Amagyei <Kwame.na@hotmail.com>
2023-10-31 06:50:40 -0700

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,18 +5,22 @@
 
 char *create_array(unsigned int size, char c)
 {
+	char *array;
+	unsigned int i;
+
 	if (size == 0)
 		return NULL;
 
-	char *array = (char *)malloc(size * sizeof(char));
+	array = (char *)malloc(size * sizeof(char));
 
-	if (array != null)
+	if (array != NULL)
 	{
-		for (int i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			array[i] = c;
-		}
+		}	
 	}
+
 
 	return array;
 }
