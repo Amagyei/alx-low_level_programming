7c485efb64ccbad0ae8f82d0e540046b517661c9
amagyei <kwame.na@hotmail.com>
2023-10-31 22:54:45 +0000

    task completed

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,19 +1,34 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
-char *_strdup(char *str) {
-    char *duplicate;
+/**
+ * _strdup - display a copy of a string in a pointer
+ * @str: string
+ * Return: Null or string
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	unsigned int i, j;
 
-    if (str == NULL) {
-        return NULL;
-    }
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	if (i < 1)
+		return (NULL);
+	p = malloc(sizeof(char) * i);
+	if (p == NULL)
+	{
+		free(p);
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+		p[j] = str[j];
+	p[j] = '\0';
 
-    duplicate = (char *)malloc(strlen(str) + 1);
-
-    if (duplicate != NULL) {
-        strcpy(duplicate, str);
-    }
-
-    return duplicate;
+	return (p);
 }
