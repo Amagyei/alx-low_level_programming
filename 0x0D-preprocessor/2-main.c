c4cff15cc36a09b72566d96a0f66454c3e0ac31b
Amagyei <Kwame.na@hotmail.com>
2023-11-06 07:58:29 -0800

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * void: 'Prints the name of the file it was printed frm'
+ *
+ * Description: Uses __FILE__  to achieve
+ *
+ * Return: 0
+ */
+
+int main()
+{
+	printf(__FILE__);
+	return (0);
+}
