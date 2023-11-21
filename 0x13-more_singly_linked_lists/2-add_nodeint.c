6d3a0c6b5f7bf9d93111e08515cd90ddb4030f14
Amagyei <Kwame.na@hotmail.com>
2023-11-21 08:58:34 -0800

    task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
