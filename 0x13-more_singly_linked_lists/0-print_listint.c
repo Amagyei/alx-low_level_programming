81151968c019d0cedb0e933013c37abc315379b8
Amagyei <Kwame.na@hotmail.com>
2023-11-21 07:24:08 -0800

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * size_t print_listint - print all the nodes in a singly linked list
+ *
+ * @h - pointer to list to be printed
+ *
+ * Description: prints each node of a list
+ *
+ * Return: 0 on success 
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+	while(h->next != NULL)
+	{
+		count++;
+		printf("%d", h->n);
+		h = h->next;
+	}
+
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+typedef struct list_s
+{
+	int n;
+    	struct list_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+
+
+#endif
