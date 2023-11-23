c05bdc1254734e7ad3c7363f9680c96603a86e27
Amagyei <Kwame.na@hotmail.com>
2023-11-22 18:17:52 -0800

    task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,33 @@
+#include  "lists.h"
+
+
+/**
+ * get_nodeint_at_index - gets data from a node at an index
+ *
+ * @head: pointer to the list
+ *
+ * @index: the index 
+ *
+ * Description: traverse eac node till you find the one we are looking for 
+ *
+ * Return: data or NULL
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	int count = 0;
+	listint_t *ptr;
+	int data;
+
+	while (count != 0)
+	{
+		*head = head-> next;
+		count ++;
+	}
+
+	if (count != 0)
+		return (NULL);
+
+	data = head->n;
+	return (data);
+}
