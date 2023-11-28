3ba64ff1bf464ef80f143d661c05479baadaf5cd
Amagyei <Kwame.na@hotmail.com>
2023-11-27 19:35:14 -0800

    main

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,25 +5,8 @@
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int ab = open(filename, O_RDONLY);
 
-	if(ab == -1)
-	{
-		perror("Error opening file");
-		exit(EXIT_FAILURE);
-	}
 
-	char buffer[4096];
+#endif 
 
-	ssize_t readBytes;
 
-	while((readBytes = read(ab, buffer, sizeof(buffer))) > 0)
-	{
-		if(write(STDOUT_FILENO, buffer, readBytes) == -1);
-		{
-			perror("Error opening file");
-			exit(EXIT_FAILURE);
-		}
-	}
-}
