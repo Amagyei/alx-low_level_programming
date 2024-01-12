1a0fd99827b33bc21028f48aeb663b752eecc0ab
amagyei <kwame.na@hotmail.com>
2024-01-12 13:06:33 -0800

    task 2g

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,8 +10,12 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h != NULL)
 	{
 		printf("%d", h->n);
-		h = h->next;
 		printf("\n");
+
+		if (h->next != NULL)
+			printf(" ");
+
+		h = h->next;
 		count++;
 	}
 
--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,13 +2,14 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	while (*head != NULL)
-	{
-		(*head)->n = n;
-		(*head)->next = *head;
-		(*head)->prev = NULL;
-		head = head;
-	}
+	dlistint_t *ptr = malloc(sizeof(dlistint_t));
+	ptr->n = n;
+	ptr->next = *head;
+	ptr->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = ptr;
+	(*head) = ptr;
 
-	return 0;
+
+return 0;
 }
