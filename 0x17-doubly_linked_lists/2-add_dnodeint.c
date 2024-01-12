75e2382399592cd0f4d360a5158ab653132e2228
amagyei <kwame.na@hotmail.com>
2024-01-11 16:40:26 -0800

    task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -2,8 +2,13 @@
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	*head->n = n;
-	*head->next = head;
-	*head->previous = NULL;
-	head = head;
+	while (*head != NULL)
+	{
+		(*head)->n = n;
+		(*head)->next = *head;
+		(*head)->prev = NULL;
+		head = head;
+	}
+
+	return 0;
 }
