6780ccd005ee4b1a144c8211a2f3843f114ff8cd
amagyei <kwame.na@hotmail.com>
2024-01-12 18:33:32 -0800

    task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list.
+ * sum_dlistint - Returns the nth node of a dlistint_t linked list.
  * @head: A pointer to the head of the doubly linked list.
- * @index: The index of the node to retrieve (starting from 0).
+ *
  *
  * Return: If the node at the given index exists, return a pointer to that node.
  *         Otherwise, return NULL.
  */
 int sum_dlistint(dlistint_t *head)
 {
-	unsigned int i;
 	int sum = 0;
 	dlistint_t *current = head;
 
-	for (i = 0; current != NULL && i < index; i++)
+	while (current != NULL)
 	{
 		sum += current->n;
 		current = current->next;
